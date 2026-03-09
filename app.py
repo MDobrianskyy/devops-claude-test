0995f3cb282ed009552b62d6a576af592e0e8268
MDobrianskyy <MDobrianskyy@sineeng.com>
2026-03-09 12:56:10 +0200

    feat: add environment variables support

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+message = os.getenv("APP_MESSAGE", "DevOps test")
+
 @app.route("/")
 def hello():
-    return {"message": "DevOps test"}
+    return {"message": message}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
