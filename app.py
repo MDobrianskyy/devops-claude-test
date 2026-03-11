1d6e0e9dcb4a1f6fc0fca50bfdb56786bc7e0cf8
MDobrianskyy <MDobrianskyy@sineeng.com>
2026-03-11 10:08:27 +0200

    feat: update default message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-message = os.getenv("APP_MESSAGE", "DevOps test")
+message = os.getenv("APP_MESSAGE", "ShopUA — deployed automatically!")
 
 @app.route("/")
 def hello():
